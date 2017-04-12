398baf844e81049d30569fa6b9d17c647938b166
Jonas M <jonasmotiejauskas@gmail.com>
2017-04-12 23:14:07 +0300

    Disables form and Connection works properly

--- ImapClient/ImapClientForm.cs
+++ ImapClient/ImapClientForm.cs
@@ -12,16 +12,19 @@ namespace ImapClient
 {
     public partial class ImapClientForm : Form
     {
-        // Connect Window UI elements
-        Label connectLabel = new Label() { Text = "Please enter server address", Font = new Font("Arial", 10, FontStyle.Bold), Anchor = AnchorStyles.None, Width = 200, TextAlign = ContentAlignment.MiddleCenter };
+        MyImapClient imapClient = new MyImapClient();
+
+        #region connectWindowElements
+        Label connectLabel = new Label() { Text = "IMAP CLIENT", Font = new Font("Arial", 32, FontStyle.Bold), Anchor = AnchorStyles.None, Width = 300, Height = 80, TextAlign = ContentAlignment.MiddleCenter };
         Button connectButton = new Button() { Text = "Connect", Width = 140, Height = 30, Visible = true, Anchor = AnchorStyles.None };
         TextBox connectAddressInput = new TextBox() { Anchor = AnchorStyles.None, Width = 140, Height = 60, MaxLength = 50};
-        Label connectErrorLabel = new Label() { Anchor = AnchorStyles.None, ForeColor = Color.Red, Font = new Font("Arial", 8, FontStyle.Bold), TextAlign = ContentAlignment.MiddleCenter, Width = 150, Height = 50 };
+        Label connectErrorLabel = new Label() { Anchor = AnchorStyles.None, ForeColor = Color.Red, Font = new Font("Arial", 8, FontStyle.Bold), TextAlign = ContentAlignment.TopCenter, Width = 150, Height = 100 };
+        #endregion connectWindowElements
 
         public ImapClientForm()
         {
             InitializeComponent();
-            Text = "Generic IMAP client";
+            this.Text = "IMAP client";
             this.CenterToScreen();
         }
 
@@ -33,7 +36,13 @@ namespace ImapClient
         #region connectWindow
         private void constructConnectWindow(String errorMessage)
         {
+            this.MinimumSize = new Size(350, 350);
             connectErrorLabel.Text = errorMessage;
+
+            connectButton.Enabled = true;
+            connectLabel.Enabled = true;
+            connectAddressInput.Enabled = true;
+            connectErrorLabel.Enabled = true;
 
             this.Controls.Add(connectButton);
             this.Controls.Add(connectLabel);
@@ -42,11 +51,11 @@ namespace ImapClient
             connectButton.Left = (this.ClientSize.Width - connectButton.Width) / 2;
             connectButton.Top = (this.ClientSize.Height - connectButton.Height + 60) / 2;
             connectLabel.Left = (this.ClientSize.Width - connectLabel.Width) / 2;
-            connectLabel.Top = (this.ClientSize.Height - connectLabel.Height - 60) / 2;
+            connectLabel.Top = (this.ClientSize.Height - connectLabel.Height - 120) / 2;
             connectAddressInput.Left = (this.ClientSize.Width - connectAddressInput.Width) / 2;
             connectAddressInput.Top = (this.ClientSize.Height - connectAddressInput.Height) / 2;
             connectErrorLabel.Left = (this.ClientSize.Width - connectErrorLabel.Width) / 2;
-            connectErrorLabel.Top = (this.ClientSize.Height - connectErrorLabel.Height + 120) / 2;
+            connectErrorLabel.Top = (this.ClientSize.Height - connectErrorLabel.Height + 200) / 2;
             connectButton.Click += ConnectButton_Click;
         }
 
@@ -56,14 +65,26 @@ namespace ImapClient
             this.Controls.Remove(connectLabel);
             this.Controls.Remove(connectAddressInput);
             this.Controls.Remove(connectErrorLabel);
+            connectButton.Click -= ConnectButton_Click;
         }
 
         private void ConnectButton_Click(object sender, EventArgs e)
         {
-            destructConnectionWindow();
+            foreach (Control ctrl in this.Controls)
+            {
+                ctrl.Enabled = false;
+            }
+            try
+            {
+                imapClient.Connect(connectAddressInput.Text);
+            }
+            catch(Exception)
+            {
+                destructConnectionWindow();
+                constructConnectWindow("Failed to connect to the server");
+            }
         }
         #endregion
-
 
     }
 }
--- ImapClient/MyImapClient.cs
+++ ImapClient/MyImapClient.cs
@@ -3,13 +3,59 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace ImapClient
 {
     class MyImapClient
     {
-        static System.Net.Sockets.TcpClient tcpc = null;
-        static System.Net.Security.SslStream ssl = null;
+        System.Net.Sockets.TcpClient tcpc = null;
+        System.Net.Security.SslStream ssl = null;
+        int bytes = -1;
+        byte[] buffer;
+        StringBuilder sb = new StringBuilder();
 
+        public void Connect(string address)
+        {
+            try
+            {
+                tcpc = new System.Net.Sockets.TcpClient();
+                if (!tcpc.ConnectAsync(address, 993).Wait(5000))
+                {
+                    throw new Exception("Connection timed out");
+                }
+
+                ssl = new System.Net.Security.SslStream(tcpc.GetStream());
+                ssl.AuthenticateAsClient(address);
+                ssl.Flush();
+
+                sb = new StringBuilder();
+                buffer = new byte[2048];
+                bytes = ssl.Read(buffer, 0, 2048);
+                sb.Append(Encoding.ASCII.GetString(buffer));
+                MessageBox.Show(sb.ToString());
+            }
+            catch (System.Net.Sockets.SocketException ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void Disconnect()
+        {
+            if (ssl != null)
+            {
+                ssl.Close();
+                ssl.Dispose();
+            }
+            if (tcpc != null)
+            {
+                tcpc.Close();
+            }
+        }
     }
 }
